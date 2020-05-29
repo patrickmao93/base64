a2b35ed295a92b1e248d6d1c4964754b7e5de5c0
patrickmao93 <patrickmao93@gmail.com>
2020-05-29 11:07:28 +0800

    tidy

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,11 +4,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import Header from "../components/Header";
 import NoScript from "../components/NoScript";
-import Welcome from "../components/Welcome";
 import Notifications from "../features/notifications/Notifications";
 import { updateOnlineStatus } from "../features/onlineStatus/onlineSlice";
-import useAutoUpdate from "../features/repos/autoUpdate";
-import RepoList from "../features/repos/RepoList";
 import UpdateNotification from "../features/update/UpdateNotification";
 import { RootState } from "./rootReducer";
 import { persistor } from "./store";
@@ -20,7 +17,6 @@ const selectRepos = (state: RootState) => state.repos;
 const selectUpdateAvailable = (state: RootState) => state.update.updateAvailable;
 
 const App = () => {
-  useAutoUpdate();
   const dispatch = useDispatch();
   const darkMode = useSelector(selectDarkMode);
   const updateAvailable = useSelector(selectUpdateAvailable);
@@ -54,7 +50,7 @@ const App = () => {
         <NoScript />
         {updateAvailable && <UpdateNotification />}
         <PersistGate loading={null} persistor={persistor}>
-          {repos.ids.length === 0 ? <Welcome addExamples={addExamples} /> : <RepoList />}
+          <div></div>
         </PersistGate>
       </Container>
       <NoSsr defer>
