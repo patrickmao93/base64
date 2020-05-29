b940572629f5e95950742fbe0935182bf8d1666e
patrickmao93 <patrickmao93@gmail.com>
2020-05-29 23:06:52 +0800

    tweek style

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,15 +1,24 @@
 import React, { FC, ChangeEventHandler } from "react";
-import { makeStyles, TextField } from "@material-ui/core";
+import { makeStyles, InputBase, fade, colors } from "@material-ui/core";
 
 interface IProps {
   value: string;
   onChange: ChangeEventHandler<HTMLTextAreaElement>;
 }
 
-const useStyles = makeStyles(() => {
+const useStyles = makeStyles((theme) => {
+  const bgBase = theme.palette.type === "dark" ? "#fff" : "#000";
   return {
     textarea: {
       height: "100%",
+      alignItems: "flex-start",
+      background: fade(bgBase, 0.04),
+      borderRadius: 3,
+      border: "1px solid transparent",
+      padding: "1rem",
+      "&::focus": {
+        borderColor: colors.blue[500],
+      },
     },
   };
 });
@@ -18,7 +27,7 @@ const Editor: FC<IProps> = (props) => {
   const { value, onChange } = props;
   const styles = useStyles();
   return (
-    <TextField
+    <InputBase
       value={value}
       onChange={onChange}
       className={styles.textarea}
