18da820d77f158d95747a9bcf392bc30a595e553
patrickmao93 <patrickmao93@gmail.com>
2020-05-29 11:05:02 +0800

    tidy up

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -1,14 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import notificationsReducer from "../features/notifications/notificationsSlice";
 import onlineReducer from "../features/onlineStatus/onlineSlice";
-import reposReducer from "../features/repos/reposSlice";
-import sortReducer from "../features/sortRepos/sortReposSlice";
 import themeReducer from "../features/theme/themeSlice";
 import updateReducer from "../features/update/updateSlice";
 
 const rootReducer = combineReducers({
-  repos: reposReducer,
-  sort: sortReducer,
   theme: themeReducer,
   notifications: notificationsReducer,
   update: updateReducer,
